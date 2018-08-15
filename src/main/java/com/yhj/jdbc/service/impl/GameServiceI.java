c0bfcd45d0c304e49092773fc3ba0d6a7e1d8d64
yanghj <1501587673@qq.com>
2018-08-16 07:55:58 +0800

    add new file

--- src/main/java/com/yhj/jdbc/service/impl/GameServiceI.java
+++ src/main/java/com/yhj/jdbc/service/impl/GameServiceI.java
@@ -9,7 +9,7 @@ public class GameServiceI implements GameService {
     @Override
     public int queryALlGames() {
         System.out.println("queryALlGames");
-        throw new RuntimeException("手动抛出的异常");
+          throw new RuntimeException("手动抛出的异常");
 //        return 0;
     }
 }
